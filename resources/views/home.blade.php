fde9007b2a1173d8fe617b85b5ea479bfa43ec21
Elvis David Quinteros Siles <dquinteros630@gmail.com>
2024-06-20 22:58:31 -0400

    implementando plantilla adminlt listo para usar

---
+++ resources/views/home.blade.php
@@ -0,0 +1,35 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Examen de Software 1</h1>
+@stop
+
+@section('content')
+    <h3>Integrantes:</h3>
+    <li>SERGIO CANAVIRI FELIX</li>
+    <li>
+        ELVIS DAVID QUINTEROS SILES
+    </li>
+    <li>
+        JHONNY LAMAS SOTO
+    </li>
+    <li>
+        INES QUIROGA CASTRO
+    </li>
+    <li>
+        JOSÉ CESAR SAÑEZ SALCE
+    </li>
+
+<p></p>
+@stop
+
+@section('css')
+    {{-- Add here extra stylesheets --}}
+    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+@stop
+
+@section('js')
+    <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
+@stop
