9b8518445d240b125ac965a08bc448b84169050c
jhonny <j3066162@gmail.com>
2024-06-22 00:42:06 -0400

    creacion de la migracion, las vistas listar y crear PRODUCTOS

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::resource('empleado','App\Http\Controllers\EmpleadoController');
+Route::resource('empleado', 'App\Http\Controllers\EmpleadoController');
+
+Route::resource('/producto', 'App\Http\Controllers\ProductoController');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
